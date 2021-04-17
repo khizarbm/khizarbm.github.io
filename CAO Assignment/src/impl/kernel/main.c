edae150559bebffd8bc49f3b293f2609f5e738f9
khizarbm <54741183+khizarbm@users.noreply.github.com>
2021-04-18 03:26:06 +0500

    Initial Commit

---
+++ CAO Assignment/src/impl/kernel/main.c
@@ -0,0 +1,11 @@
+#include "print.h"
+
+void kernel_main() {
+    print_clear();
+    print_set_color(PRINT_COLOR_GREEN, PRINT_COLOR_BLACK);
+    print_str("******   ******   ******  ******   ******\n");
+    print_str("*        *        *       *        *     \n");
+    print_str("******   ******   ******  *        ******\n");
+    print_str("     *   *        *       *             *\n");
+    print_str("******   ******   ******  ******   ******\n");
+}
